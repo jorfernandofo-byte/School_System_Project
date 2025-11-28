702806b83ca5ff6cfa9355e4e51d05017e92af30
Fernando <jorfernandofo@gmail.com>
2025-11-27 18:57:36 -0600

    Se agrega clase Student y se hacen los metodos

---
+++ src/Courses.java
@@ -0,0 +1,25 @@
+
+public class Courses {
+
+	   public void enroll(Student student){
+		      //TODO add the student to the collection
+		   }
+
+		   public void unEnroll(Student student){
+		       //TODO remove this student from the collection
+		       // Hint: check if that really is this student
+		   }
+
+		   public int countStudents(){
+		       //TODO implement
+		       return 0;
+		   }
+		   
+		   public int bestGrade(){
+		       //TODO implement
+		       return 0;
+		   }
+	
+	
+	
+}
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,42 @@
 
 public class Student {
+	       String firstName;
+	       String lastName;
+	       int registration;
+	       int grade;
+	       int year;
+	       
+	    
+	       
+	       
+	       
+	       public void printFullName(){
+	    	     System.out.println("Nombre:" + firstName +  "\nApellidos:" + lastName );
+	    	   }
 
+	    	   public boolean  isApproved(){
+	    	        if (grade >= 60) {
+	    	        	System.out.println("La calificacion es mayor al 60%");
+	    	         return  true;
+	    	        	
+	    	        }
+	    	        else {
+	    	        	System.out.println("La calificacion es menor al 60%");
+	    	        	return false;
+	    	        }
+	    	   }
+
+	    	   public int changeYearIfApproved(){
+	    		   if (grade >= 60) {
+	    			   System.out.println("Felicidades alumno pasas al siguiente año");
+	    			   return year+1;
+	    			  
+	    		   }
+	    	       return 0;
+	    	   }
+	       
+	       
+	       
+	       
+	  
 }
